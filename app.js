245d44df1dd560abf97864364dced720c377f9e0
Himanshu Verma <hemuv1942@gmail.com>
2020-10-03 07:34:35 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+     var myapp = new Vue({
+            el:"#our-app",
+            data:{
+                message:"This is our first app",
+                fname:"Himanshu",
+                "add":"Gwalior"
+            }
+        });
